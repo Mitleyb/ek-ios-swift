f63fc380133bfc3dbf87fb68ca555dde2a13c9f2
Eilon Krauthammer <mitleyber@gmail.com>
2020-05-12 17:37:30 +0300

    Add files via upload

---
+++ Services/AuthenticationService.swift
@@ -0,0 +1,34 @@
+//
+//  AuthenticationManager.swift
+//  Dating
+//
+//  Created by Eilon Krauthammer on 30/03/2020.
+//  Copyright © 2020 Eilon Krauthammer. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+struct LocalAuth {
+    static func authenticate(successHandler: @escaping (Bool) -> Void) {
+        let context = LAContext()
+        var error: NSError?
+        
+        let authReason = localized("auth_reason")
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason) { success, error in
+                DispatchQueue.main.async {
+                    successHandler(success)
+                }
+            }
+        } else {
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: authReason) { success, error in
+                DispatchQueue.main.async {
+                    successHandler(success)
+                }
+            }
+        }
+    }
+}
+
+
